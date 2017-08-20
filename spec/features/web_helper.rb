cc79c438d587b809a3d565d75f5e41b36c9ea662
AaronRodrigues <aaronrod@gmail.com>
2017-08-20 23:41:07 +0100

    Refactors tests

--- spec/features/web_helper.rb
+++ spec/features/web_helper.rb
@@ -1,6 +1,6 @@
 def add_recipient
   visit('/')
   click_button "Make a payment"
-  fill_in :recipient, with: "Bruce Wayne"
+  fill_in :recipient, with: "Guybrush Threepwood"
   click_button "Add Recipient"
 end
