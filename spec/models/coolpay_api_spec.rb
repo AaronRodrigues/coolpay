9a6d6123e50802a79441744d3f8342f3e4b145d3
AaronRodrigues <aaronrod@gmail.com>
2017-08-20 21:29:35 +0100

    Creates API Class -Refactors tests -Creates webhelper

--- spec/models/coolpay_api_spec.rb
+++ spec/models/coolpay_api_spec.rb
@@ -1,13 +1,14 @@
 require './app/models/coolpay_api'
 
 describe CoolpayAPI do
-  let(:http) { double(:http, request: { "token": "aff06fec-e041-4994-849e-223f0569e0bc" })}
+  subject(:api) { described_class.new(http_client_class: http_client_class, http_request_class: http_request_class) }
+  let(:http) { double(:http, request: { "token": "0a1111-101010-10010-101a10-1111" })}
   let(:http_client) { double(:http_client, get_request_uri: '/login', http: http)}
   let(:http_client_class) { double(:http_client_class, new: http_client) }
   let(:http_request) { double(:http_request, get_body: credentials, build: nil)}
   let(:http_request_class) { double(:http_request_class, new: http_request) }
   let(:credentials) { { "username": "your_username", "apikey": "5up3r$ecretKey!" } }
-  subject(:api) { described_class.new(http_client_class: http_client_class, http_request_class: http_request_class) }
+  
 
   describe('#send_request') do
     it('creates a new client') do
--- spec/models/http_request_spec.rb
+++ spec/models/http_request_spec.rb
@@ -2,23 +2,23 @@ require './app/models/request'
 
 describe Request do
   let(:login_uri) { "/api/login" }
-  let(:token) { 'you_are_cool'}
+  let(:token) { 'secret'}
   let(:credentials) { {"username": "your_username", "apikey": "5up3r$ecretKey!"} }
   subject(:post) { described_class.new(type: 'POST', uri: login_uri) }
   subject(:get) { described_class.new(type: 'GET', uri: login_uri)}
 
   describe '#request' do
-    context 'POST type' do
+    context 'POST' do
       it('returns a post request when the given type is POST') do
         expect(post.request).to be_kind_of(Net::HTTP::Post)
       end
 
-      it('has a content-type header') do
+      it('has a header') do
         expect(post.request['content-type'.to_sym]).to eq 'application/json'
       end
     end
 
-    context 'GET type' do
+    context 'GET' do
       it('returns a get request when the given type is GET') do
         expect(get.request).to be_kind_of(Net::HTTP::Get)
       end
@@ -31,35 +31,35 @@ describe Request do
 
   describe '#build' do
     context 'post request' do
-      it('stores the given message in the body') do
+      it('stores credentials in body') do
         post.build(message: credentials)
         expect(post.get_body).to eq(credentials.to_json)
       end
 
-      it('has an authorization header if token is provided') do
+      it('has an authorization header') do
         post.build(token: token)
         expect(post.request['authorization']).to eq "Bearer #{token}"
       end
 
-      it('does not have an authorization header if token is not provided') do
+      it('is nil when not authorized') do
         post.build
         expect(post.request['authorization']).to eq nil
       end
     end
 
-    context 'get request' do
-      it('does not have a body') do
+    context 'GET request' do
+      it('has no body') do
         get.build
         expect(get.get_body).to eq(nil)
       end
     end
 
-    it('has an authorization header if token is provided') do
+    it('has a header if passes authorization') do
       get.build(token: token)
       expect(get.request['authorization']).to eq "Bearer #{token}"
     end
 
-    it('does not have an authorization header if token is not provided') do
+    it('does not have a header if fails authorization') do
       get.build
       expect(get.request['authorization']).to eq nil
     end
